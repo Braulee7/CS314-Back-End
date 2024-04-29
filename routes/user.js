58e949d9b07fafcd15a60369d1172f5ac71f66a6
Braulee7 <braulio00001@gmail.com>
2024-04-29 06:27:02 -0700

    Finished creating user endpoint

--- routes/user.js
+++ routes/user.js
@@ -9,8 +9,24 @@ const router = express.Router();
 
 // dependency injection with the database for testing purposes
 export default function (database) {
+  // POST localhost:3333/user\
+  // body: username {string} password {string}
+  // response:
+  //    on success the route will return the username of the
+  //    user creating an account. This will act as the UID.
+  //    on failure the route will return a string containing
+  //    the PSQL error response. The most common situation will
+  //    be for a a non-unique value (i.e. username already taken)
   router.post("/", async (req, res) => {
     const { username, password } = req.body;
+    // make sure we got valid inputs
+    if (
+      !username ||
+      !password ||
+      typeof username != "string" ||
+      typeof password != "string"
+    )
+      res.status(400).send("Need a valid username and password");
     // make a request to the database to create user
     try {
       const userid = await database.createUser(username, password);
--- tests/user.test.js
+++ tests/user.test.js
@@ -26,10 +26,7 @@ describe("POST /user", () => {
         password: "password",
       };
       // ACT
-      const response = await request(app).post("/user").send({
-        username: "username",
-        password: "password",
-      });
+      const response = await request(app).post("/user").send(user);
 
       // ASSERT
       expect(response.statusCode).toBe(201);
@@ -52,6 +49,40 @@ describe("POST /user", () => {
         // ASSERT
         expect(createUser.mock.calls[i][0]).toBe(user);
       });
+    });
+  });
+
+  describe("Registering with bad inputs", () => {
+    it("Give a 400 response status", async () => {
+      // ARRANGE
+      createUser.mockReset();
+      createUser.mockResolvedValue(-1); // error
+      // mock username and password
+      const username = "user";
+      const password = "password";
+
+      // ACT
+      // test three scenarios
+      // registering with out a username or password
+      const response1 = await request(app).post("/user");
+      // registering without a password
+      const response2 = await request(app)
+        .post("/user")
+        .send({ username: username });
+      // registering without a username
+      const response3 = await request(app)
+        .post("/user")
+        .send({ password: password });
+      // invalid types
+      const response4 = await request(app)
+        .post("/user")
+        .send({ username: 123, password: password });
+
+      // ASSERT
+      expect(response1.statusCode).toBe(400);
+      expect(response2.statusCode).toBe(400);
+      expect(response3.statusCode).toBe(400);
+      expect(response4.statusCode).toBe(400);
     });
   });
 });
